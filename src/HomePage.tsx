e3c3c33d79a45427da3094c66efd3c3d3a3fa143
gauravlitmusblox <gaurav@litmusblox.io>
2023-03-22 19:06:44 +0530

    fix: add a search boxes

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Banner } from "./component/Banner";
+import SearchBox from "./component/SearchBoxes";
 
 const HomePage = () => {
+  const [searchStatusQuery, setSearchStatusQuery] = useState("");
+  const [searchOriginalLaunchQuery, setSearchOriginalLaunchQuery] =
+    useState("");
+  const [searchTypeQuery, setSearchTypeQuery] = useState("");
   return (
     <div>
       <Banner />
+      <SearchBox
+        setSearchStatusQuery={setSearchStatusQuery}
+        setSearchOriginalLaunchQuery={setSearchOriginalLaunchQuery}
+        setSearchTypeQuery={setSearchTypeQuery}
+      />
     </div>
   );
 };
